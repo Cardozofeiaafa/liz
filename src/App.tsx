7ef146c51cb83ea312e59351bb6d0544f65b4414
Cardozofeiaafa <andreicardozo400@gmail.com>
2024-12-07 14:00:05 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { CountdownTimer } from './components/CountdownTimer';
 import { BirthdayPage } from './components/BirthdayPage';
 
