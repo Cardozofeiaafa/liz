407a19fb12df66bbf34bfbb6b0e354b58a51166e
Cardozofeiaafa <andreicardozo400@gmail.com>
2024-12-07 13:58:35 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -3,17 +3,23 @@ import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      react.configs.recommended, // Adicione o plugin react
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
+      'react': react, // Adicione o plugin aqui
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
@@ -23,6 +29,7 @@ export default tseslint.config(
         'warn',
         { allowConstantExport: true },
       ],
+      'react/react-in-jsx-scope': 'off', // Desative a necessidade de importar React
     },
   }
 );
