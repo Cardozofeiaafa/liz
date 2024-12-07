6fbe0318ab11dbe0ecc445db7f8a7af5088066e6
Cardozofeiaafa <andreicardozo400@gmail.com>
2024-12-07 14:00:33 -0300

    Update BirthdayPage.tsx

--- src/components/BirthdayPage.tsx
+++ src/components/BirthdayPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import from 'react';
 import { TextReveal } from './TextReveal';
 import { BackgroundMusic } from './BackgroundMusic';
 import { Heart } from 'lucide-react';
