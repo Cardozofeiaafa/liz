3993431d6a09dca602375853e483a3b8f072ab65
Cardozofeiaafa <andreicardozo400@gmail.com>
2024-12-07 14:00:22 -0300

    Update BackgroundMusic.tsx

--- src/components/BackgroundMusic.tsx
+++ src/components/BackgroundMusic.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Volume2, VolumeX } from 'lucide-react';
 
 export function BackgroundMusic() {
